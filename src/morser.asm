5af40b164fecdf86c48a0ec0f1cd51b386fb8543
Toaster996 <32509408+Toaster996@users.noreply.github.com>
2018-06-06 11:19:11 +0200

    Generate Bitmask in R2

--- src/morser.asm
+++ src/morser.asm
@@ -1,38 +1,60 @@
-TIMER_START EQU 42d
-BUTTON EQU P0.0
-
 cseg at 0h
 jmp INIT
 
 ; Wait for first user input
-org 10h
+org 0bh ; adresse v on timer interrupt
+call LONG_PRESS_DETECED
+reti ; return from interupt
+
+org 1bh 
+call TIME_OVER
+reti
+
+
 INIT:
- 	MOV IE, #10000010b
+	TIMER_START EQU 42d
+	BUTTON EQU P0.0
+
+ 	MOV IE, #10001010b ;enables interupt
+ 	MOV R0, #1h
+ 	MOV R5, #1h
 	JMP BTN_LOOP
 
- BTN_LOOP:;!pressed
+BTN_LOOP:; !pressed
  	clr tr0
- 	;if(R1==1){
- 	; set R7 for long press}
- 	;else for shortpress
 
+	CJNE R0, #1h, ANALYZE_PREV
  	;Run timer to detect break
+ 	CJNE R5, #0h, START_WAIT_TIMER
  	JNB BUTTON, START_TIMER
  	JMP BTN_LOOP
 
- PRESSED:
- 	JNB BUTTON, pressed
+PRESSED:
+ 	MOV R0, #0
+ 	CLR TR1
+ 	JNB BUTTON, PRESSED
+ 	MOV R5, #1h
  	JB BUTTON, BTN_LOOP
  
 
 START_TIMER:
-	mov TMOD, #00000010b
-	mov tl1, #000h ; working #0C0h
-	mov th1, #000h ; working #0C0h
-	mov tl0, #000h ; working #0C0h
-	mov th0, #000h ; working #0C0h
+	MOV TMOD, #00000010b
+	mov tl1, #0C0h ; working #0C0h
+	mov th1, #0C0h ; working #0C0h
+	mov tl0, #0C0h ; working #0C0h
+	mov th0, #0C0h ; working #0C0h
  	setb tr0 ; startet timer
 	JMP PRESSED
+
+START_WAIT_TIMER:
+	MOV R5, #0
+	MOV TMOD, #00000010b
+	mov tl1, #0F0h ; working #0C0h
+	mov th1, #0F0h ; working #0C0h
+	mov tl0, #0F0h ; working #0C0h
+	mov th0, #0F0h ; working #0C0h
+ 	setb tr1 ; startet timer
+	JMP BTN_LOOP
   
 
 LONG_PRESS_DETECED:
@@ -40,6 +62,34 @@ LONG_PRESS_DETECED:
 	clr tr0 ;stop timer
 	RET
 
-org 0bh ; adresse v on timer interrupt
-call LONG_PRESS_DETECED
-reti ; return from interupt
+ANALYZE_PREV:
+	MOV A, R2
+	CJNE R1, #1h, SHORT_PRESS; if (not longpress)
+	CJNE R1, #0h, LONG_PRESS;
+
+
+ANALYZE_PREV2:
+	MOV R2, A
+	MOV R0, #1
+	MOV R1, #0
+	MOV R5, #1h
+	JMP BTN_LOOP
+		
+LONG_PRESS:
+	RL A
+	SETB A.0
+	RL A
+	JMP ANALYZE_PREV2
+
+SHORT_PRESS:
+	RL A
+	RL A
+	SETB A.0
+	JMP ANALYZE_PREV2
+
+TIME_OVER:
+CLR t1
+RL A
+;call Phills stuff
+RET
+	
